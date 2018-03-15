c6490922a1875b520370ba8dfc9c8c3fd178ee70
Lior Goldstein <gs.lior@gmail.com>
2018-03-15 18:24:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,151 @@
-$(document).ready(function() {
-    $(".onboarding").click(function(){
-        $(this).fadeOut("slow");
-    });
-})
-$( document ).on( "pagecreate", "#demo-page", function() {
-    // Swipe to remove list item
-    $( document ).on( "swipeleft swiperight", "#list li", function( event ) {
-        var listitem = $( this ),
-            // These are the classnames used for the CSS transition
-            dir = event.type === "swipeleft" ? "left" : "right",
-            // Check if the browser supports the transform (3D) CSS transition
-            transition = $.support.cssTransform3d ? dir : false;
-            confirmAndDelete( listitem, transition );
-    });
-    // If it's not a touch device...
-    if ( ! $.mobile.support.touch ) {
-        // Remove the class that is used to hide the delete button on touch devices
-        $( "#list" ).removeClass( "touch" );
-        // Click delete split-button to remove list item
-        $( ".delete" ).on( "click", function() {
-            var listitem = $( this ).parent( "li" );
-            confirmAndDelete( listitem );
-        });
-    }
-    function confirmAndDelete( listitem, transition ) {
-        // Highlight the list item that will be removed
-        listitem.children( ".ui-btn" ).addClass( "ui-btn-active" );
-        // Inject topic in confirmation popup after removing any previous injected topics
-        $( "#confirm .topic" ).remove();
-        listitem.find( ".topic" ).clone().insertAfter( "#question" );
-        // Show the confirmation popup
-        //$( "#confirm" ).popup( "open" );
-        // Proceed when the user confirms
-        
-        //$( "#confirm #yes" ).on( "click", function() {
-            // Remove with a transition
-            if ( transition ) {
-                listitem
-                    // Add the class for the transition direction
-                    .addClass( transition )
-                    // When the transition is done...
-                    .on( "webkitTransitionEnd transitionend otransitionend", function() {
-                        // ...the list item will be removed
-                        listitem.remove();
-                        // ...the list will be refreshed and the temporary class for border styling removed
-                        $( "#list" ).listview( "refresh" ).find( ".border-bottom" ).removeClass( "border-bottom" );
-                    })
-                    // During the transition the previous button gets bottom border
-                    .prev( "li" ).children( "a" ).addClass( "border-bottom" )
-                    // Remove the highlight
-                    .end().end().children( ".ui-btn" ).removeClass( "ui-btn-active" );
-            }
-            // If it's not a touch device or the CSS transition isn't supported just remove the list item and refresh the list
-            else {
-                listitem.remove();
-                $( "#list" ).listview( "refresh" );
-            }
-        //});
-        
-        // Remove active state and unbind when the cancel button is clicked
-        $( "#confirm #cancel" ).on( "click", function() {
-            listitem.children( ".ui-btn" ).removeClass( "ui-btn-active" );
-            $( "#confirm #yes" ).off();
-        });
-    }
-});
+/* Left transition */
+li.left {
+    -webkit-transition: -webkit-transform 250ms ease;
+    -webkit-transform: translateX(-100%);
+    -moz-transition: -moz-transform 250ms ease;
+    -moz-transform: translateX(-100%);
+    -o-transition: -o-transform 250ms ease;
+    -o-transform: translateX(-100%);
+    transition: transform 250ms ease;
+    transform: translateX(-100%);
+}
+/* Right transition */
+li.right {
+    -webkit-transition: -webkit-transform 250ms ease;
+    -webkit-transform: translateX(100%);
+    -moz-transition: -moz-transform 250ms ease;
+    -moz-transform: translateX(100%);
+    -o-transition: -o-transform 250ms ease;
+    -o-transform: translateX(100%);
+    transition: transform 250ms ease;
+    transform: translateX(100%);
+}
+/* Border bottom for the previous button during the transition*/
+li.left a.ui-btn,
+li.right a.ui-btn {
+    border-top-width: 0;
+    border-left-width: 1px;
+    border-right-width: 1px;
+}
+li a.ui-btn.border-bottom {
+    border-bottom-width: 1px;
+}
+/* Hide the delete button on touch devices */
+ul.touch li.ui-li-has-alt a.delete {
+    display: none;
+}
+ul.touch li.ui-li-has-alt a.ui-btn {
+    margin-right:0;
+}
+/* Styling for the popup */
+#confirm p {
+    text-align: center;
+    font-size: inherit;
+    margin-bottom: .75em;
+}
+
+/* Added */
+
+.onboarding {
+    position: fixed;
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: #f35556;
+    text-align: center;
+    z-index:100;
+    font-size: 5em;
+}
+
+.startBtn {
+    border: 1px solid #000;
+    width: 20%;
+    margin: 200px auto;
+    padding: 50px;
+    border-radius: 100px;
+}
+
+.beenderHeader {
+    position: relative;
+}
+
+.beenderHeader img {
+    position: absolute;
+}
+
+.setting {
+    top: 20px;
+    left: 2%;
+}
+
+.logo {
+    left:0;
+    right:0;
+    margin: 0 auto;
+    width: 35%;
+    max-width: 300px;
+}
+
+.ui-page {
+    margin-top: 9%;
+}
+
+#list li {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+}
+
+.ui-btn {
+    
+    padding: 0 !important;
+    margin: 0 !important;
+}
+
+.ui-btn img {
+    position: relative !important;
+    left: 0 !important;
+    top: 0 !important;
+    max-height: none !important;
+    max-width: none !important;
+    width:100%;
+    min-height: 100%;
+}
+
+.imgWrapper {
+    padding: 30px;
+}
+
+.topic {
+    font-size: 4em !important;
+    padding: 0;
+    margin: 0 3% !important;
+    text-align: right;
+}
+
+.delete {
+    top: auto !important;
+    bottom: 0 !important;
+    height: 20px !important;
+    width: 100% !important;
+}
+
+.bottomBtn {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.bottomBtn img {
+    width: 100%;
+    max-width: 1000px;
+}
+
+@media only screen 
+  and (min-device-width: 500px) {
+      #list li {
+            max-width: 28em;
+        }
+}
